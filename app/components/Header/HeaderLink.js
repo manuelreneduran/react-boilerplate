8bcd8cc23d20f73f256be21cf93094f2e904f767
Manuel Duran <mduran85@gmail.com>
2020-08-25 14:44:44 -0500

    added styles to nav buttons

--- app/components/Header/HeaderLink.js
+++ app/components/Header/HeaderLink.js
@@ -6,7 +6,7 @@ export default styled(Link)`
   padding: 0.25em 2em;
   margin: 1em;
   text-decoration: none;
-  border-radius: 4px;
+  border-radius: 10px;
   -webkit-font-smoothing: antialiased;
   -webkit-touch-callout: none;
   user-select: none;
@@ -15,11 +15,11 @@ export default styled(Link)`
   font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif;
   font-weight: bold;
   font-size: 16px;
-  border: 2px solid #41addd;
-  color: #41addd;
+  color: lightcoral;
+  border: 1px solid lightcoral;
 
   &:active {
-    background: #41addd;
-    color: #fff;
+    background: lightcoral;
+    color: white;
   }
 `;
